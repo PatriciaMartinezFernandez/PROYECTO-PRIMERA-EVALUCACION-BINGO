1db85b046d8c11eb86a2729e852b09aed921b605
PatriciaMartinezFernandez <145768754+PatriciaMartinezFernandez@users.noreply.github.com>
2023-12-18 18:40:02 +0100

    v2 Generación Cartón & Tachado bingo
    El cartón ya no se "desmonta" al salir números del 0 al 9 y reemplezado los 0 a X a la hora de tachar

--- src/main/bingo.java
+++ src/main/bingo.java
@@ -1,75 +1,78 @@
 package main;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class bingo {
 
 	static Scanner sc = new Scanner(System.in);
-	
-	//Conteo de rondas
+
+	// Conteo de rondas
 	public static int Rondas(int contador) {
 		contador++;
 		return contador;
 	}
-	
-	//Generar número aleatorio entre 1 y 89 y no se repiten
+
+	// Generar número aleatorio entre 1 y 89 y no se repiten
 	public static int GenerarNumeroUnico(int[] numerosMostrados) {
-	    int numeroRandom;
+		int numeroRandom;
 
-	    do {
-	        numeroRandom = (int) (Math.random() * 89) + 1;
-	    } while (numerosMostrados[numeroRandom] != 0);
+		do {
+			numeroRandom = (int) (Math.random() * 89) + 1;
+		} while (numerosMostrados[numeroRandom] != 0);
 
-	    numerosMostrados[numeroRandom] = numeroRandom;
-	    return numeroRandom;
+		numerosMostrados[numeroRandom] = numeroRandom;
+		return numeroRandom;
 	}
-	
 
-	//Comprobación del número aleatorio con el carton
-	public static int [][]CartonComprobacion(int matriz[][],int aleatorio){
-		
-		int [][] carton=matriz;
-		int [][] cartonComprobado=new int [3][9];
-		
-		//Se pasa todos los valores a la nueva matriz
-		for(int fila=0;fila<carton.length;fila++) {
-			for(int columna=0;columna<carton[fila].length;columna++) {
-				cartonComprobado[fila][columna]=carton[fila][columna];
+	// Comprobación del número aleatorio con el carton
+	public static int[][] CartonComprobacion(int matriz[][], int aleatorio) {
+
+		int[][] carton = matriz;
+		int[][] cartonComprobado = new int[3][9];
+
+		// Se pasa todos los valores a la nueva matriz
+		for (int fila = 0; fila < carton.length; fila++) {
+			for (int columna = 0; columna < carton[fila].length; columna++) {
+				cartonComprobado[fila][columna] = carton[fila][columna];
 			}
 		}
-				
-		for(int fila=0;fila<cartonComprobado.length;fila++) {
-			for(int columna=0;columna<cartonComprobado[fila].length;columna++) {
-				if(cartonComprobado[fila][columna]==aleatorio) {
-					cartonComprobado[fila][columna]=0;
+
+		for (int fila = 0; fila < cartonComprobado.length; fila++) {
+			for (int columna = 0; columna < cartonComprobado[fila].length; columna++) {
+				if (cartonComprobado[fila][columna] == aleatorio) {
+					cartonComprobado[fila][columna] = 0;
 				}
 			}
 		}
-		
+
 		return cartonComprobado;
 	}
-	
-	//Comprueba que todos los números del carton están a 0 y termina el juego
+
+	// Comprueba que todos los números del carton están a 0 y termina el juego
 	public static boolean CartonCompleto(int matriz[][]) {
-		
-		boolean completo=true;
-		
-		for(int fila=0;fila<matriz.length;fila++) {
-			for(int columna=0;columna<matriz[fila].length && completo;columna++) {
-				if(matriz[fila][columna]!=0) {
-					completo=false;
+
+		boolean completo = true;
+
+		for (int fila = 0; fila < matriz.length; fila++) {
+			for (int columna = 0; columna < matriz[fila].length && completo; columna++) {
+				if (matriz[fila][columna] != 0) {
+					completo = false;
 				}
 			}
 		}
 		return completo;
 	}
-	
-	
+
 	public static int[][] imprimeMatriz(int matriz[][]) {
 		for (int i = 0; i < matriz.length; i++) {
 			System.out.println();
 			for (int j = 0; j < matriz[0].length; j++) {
-				System.out.print(matriz[i][j] + " ");
+				if (matriz[i][j] == 0) {
+	                System.out.print(" X ");
+	            } else {
+	                System.out.printf("%2d ", matriz[i][j]);
+	            }
 			}
 		}
 		return matriz;
@@ -88,22 +91,21 @@ public class bingo {
 	public static void main(String[] args) {
 
 		int[][] carton = new int[3][9];
-		int[]numerosMostrados=new int[90];
-		int menu,contador=0,aleatorio=0;
-		
-		
-		System.out.println("=========================");
+		int[] numerosMostrados = new int[90];
+		int menu, contador = 0, aleatorio = 0;
+
+		System.out.println("===========================");
 		System.out.println("   BIENVENIDO AL BINGO   ");
-		System.out.println("=========================");
+		System.out.println("===========================");
 		System.out.println("1) Empezar partida");
 		System.out.println("2) Salir");
-		System.out.println("=========================");
+		System.out.println("===========================");
 		menu = sc.nextInt();
-		
+
 		System.out.println("\n");
 
 		if (menu == 1) {
-			
+
 			for (int i = 0; i < carton.length; i++) {
 				for (int j = 0; j < carton[0].length; j++) {
 					if (j == 0) {
@@ -112,20 +114,19 @@ public class bingo {
 							numero = (int) (Math.random() * 9) + 1;
 						} while (existeNumeroEnColumna(carton, numero, j));
 						carton[i][j] = numero;
-					}
-					else {
+					} else {
 						// Generar números en el rango sin que se repita
 						int numero;
 						do {
 							numero = (int) (Math.random() * 10) + (j * 10);
 						} while (existeNumeroEnColumna(carton, numero, j));
 						carton[i][j] = numero;
-			
+
 					}
 
 				}
 			}
-			
+
 			// Ordenar los números en cada columna
 			for (int j = 0; j < carton[0].length; j++) {
 				int[] columna = new int[3];
@@ -137,36 +138,27 @@ public class bingo {
 					carton[i][j] = columna[i];
 				}
 			}
-			
-			System.out.println("=========================");
+
+			System.out.println("===========================");
 			System.out.println("\tB I N G O");
 			imprimeMatriz(carton);
-			
-			
+
 			do {
-				System.out.println("\n\n=========================");
-				contador=Rondas(contador);
-				System.out.println("Ronda: "+contador);
-				aleatorio=(GenerarNumeroUnico(numerosMostrados));
-				System.out.println("Ha salido el "+aleatorio);
-				System.out.println("\n\n=========================");
-				carton=CartonComprobacion(carton,aleatorio);
+				System.out.println("\n\n===========================");
+				contador = Rondas(contador);
+				System.out.println("Ronda: " + contador);
+				aleatorio = (GenerarNumeroUnico(numerosMostrados));
+				System.out.println("Ha salido el " + aleatorio);
+				System.out.println("\n\n===========================");
+				System.out.println("\tB I N G O");
+				carton = CartonComprobacion(carton, aleatorio);
 				imprimeMatriz(carton);
-				System.out.println("\n\n=========================");
-				
-				
-			}
-			while(!CartonCompleto(carton));
+				System.out.println("\n\n===========================");
+
+			} while (!CartonCompleto(carton));
 			System.out.println();
-			
-			
-			
-			
-			
-			
-			
+
 		}
-		
 
 		sc.close();
 	}
